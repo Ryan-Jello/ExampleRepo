0359c54b901a7e7aecdf0fdd1c008df6bd53f47b
Ryan Jellison <rjellison924@gmail.com>
2024-10-15 13:21:17 -0500

    This is my first commit!

---
+++ ExamplePython.py
@@ -0,0 +1,12 @@
+a = "String 1"
+b = "String 2"
+print("a =" + a + " and b=" + b)
+
+
+#This syntax utilizes the concept of the f-string, which makes things easier. No + sign needed!
+x = "String 3"
+y = "String 4"
+print(f"x = {x} and y = {y}")
+
+#We could also use an f-string like this:
+print("c = {c} and d = {d}".format(c=1, d=2))
